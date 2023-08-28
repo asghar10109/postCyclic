ca7bb6ffdc91d995bec393804af5f4e895a7356e
Muhammad Asghar <muhammad.asghar@technado.pk>
2023-08-28 17:50:08 +0500

    cyclic ......

--- controllers/api/authController.js
+++ controllers/api/authController.js
@@ -764,6 +764,8 @@ const socialLogin = async (req, res) => {
 /** Complete User Profile **/
 const completeProfile = async (req, res) => {
     // console.log(req.files.user_image[0].path);
+    let filename = req.path.slice(1)
+    console.log(filename)
     try {
 
         if (!req.body.id) {
